7ee6811a20a554cd54dc12bc7d99c4974d08b79d
“Dinesh” <yeligindladinesh@gmail.com>
2021-01-19 08:53:17 -0600

    Updated Todo table

--- Todo/todo/src/app/list-todos/list-todos.component.ts
+++ Todo/todo/src/app/list-todos/list-todos.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-todos.component.scss']
 })
 export class ListTodosComponent implements OnInit {
-
+todos = [
+  {id:1,description:'task1'},
+  {id:2,description:'task2'},
+  {id:3,description:'task3'}
+]
   constructor() { }
 
   ngOnInit(): void {
